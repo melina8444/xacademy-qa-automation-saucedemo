9d25ced21fd912b8db5334ec9881d4ffb468239e
melina8444 <melinayanguez@gmail.com>
2026-06-01 20:55:28 -0300

    Agregue para validar que estaba en la pag inventory.html

--- cypress/e2e/Login/logoutMenuHambur.cy.js
+++ cypress/e2e/Login/logoutMenuHambur.cy.js
@@ -8,7 +8,9 @@ describe('Pagina principal', () => {
         cy.get('#user-name').type('standard_user') // Ingreso el nombre de usuario
         cy.get('#password').type('secret_sauce') // Ingreso la contraseña
         cy.get('#login-button').click() // Hago clic en el botón de Login
-    
+
+        cy.url().should('include','/inventory.html')//valido que estoy en la pagina "inventory.html"
+        
         cy.get('#react-burger-menu-btn').click() // Hago clic en el botón del menú hamburguesa
         cy.get('#logout_sidebar_link').click() // Hago clic en el enlace de Logout
     })
